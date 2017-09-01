a7e634d0dfc49b49759bcbedfa80357c17114d48
Owen Thomas <owenthomas14@hotmail.co.uk>
2017-09-01 17:44:49 +0100

    Updated Game Detail View

--- src/GameGallery/Controllers/GameController.cs
+++ src/GameGallery/Controllers/GameController.cs
@@ -10,6 +10,17 @@ namespace GameGallery.Controllers
         { 
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Grand Theft Auto 5";
+            ViewBag.Ranking = 1;
+            ViewBag.Description = "<p>The biggest, most dynamic and most diverse open world ever created, Grand Theft Auto V blends storytelling and gameplay in new ways as players repeatedly jump in and out of the lives of the game’s three lead characters, playing all sides of the game’s interwoven story.<strong></p>";
+            ViewBag.Creaters = new string[]
+            {
+                "Developer: Rockstar North",
+                "Publisher: Rockstar Games",
+                "Series: Grand Theft Auto",
+                "Platform: Xbox One, Playstation 4, PC"
+            };
+     
             return View();
         }
     }
