dba62d180badb703d86af1b07be7a11253cdb4fe
Owen Thomas <owenthomas14@hotmail.co.uk>
2017-09-01 19:24:12 +0100

    Added Game and creater models

---
+++ src/GameGallery/Models/Creater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameGallery.Models
+{
+    public class Creater
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ src/GameGallery/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameGallery.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int Ranking { set; get; }
+        public string DescriptionHtml { set; get; }
+        public Creater[] Creaters { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
