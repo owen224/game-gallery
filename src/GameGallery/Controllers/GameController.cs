287c945fd311fc14963bda7d25db8ac233f93244
Owen Thomas <owenthomas14@hotmail.co.uk>
2017-09-03 23:32:16 +0100

    Updated Game Detail view to be strongly typed

--- src/GameGallery/Controllers/GameController.cs
+++ src/GameGallery/Controllers/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,18 +11,21 @@ namespace GameGallery.Controllers
         { 
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "Grand Theft Auto 5";
-            ViewBag.Ranking = 1;
-            ViewBag.Description = "<p>The biggest, most dynamic and most diverse open world ever created, Grand Theft Auto V blends storytelling and gameplay in new ways as players repeatedly jump in and out of the lives of the game’s three lead characters, playing all sides of the game’s interwoven story.<strong></p>";
-            ViewBag.Creaters = new string[]
+            var game = new Game()
             {
-                "Developer: Rockstar North",
-                "Publisher: Rockstar Games",
-                "Series: Grand Theft Auto",
-                "Platform: Xbox One, Playstation 4, PC"
+                SeriesTitle = "Grand Theft Auto 5",
+                Ranking = 1,
+                DescriptionHtml = "<p>The biggest, most dynamic and most diverse open world ever created, Grand Theft Auto V blends storytelling and gameplay in new ways as players repeatedly jump in and out of the lives of the game’s three lead characters, playing all sides of the game’s interwoven story.<strong></p>",
+                Creaters = new Creater[]
+                {
+                    new Creater() { Name = "Rockstar North", Role = "Developer" },
+                    new Creater() { Name = "Rockstar Games", Role = "Publisher" },
+                    new Creater() { Name = "Grand Theft Auto", Role = "Series" },
+                    new Creater() { Name = "Xbox One, Playstation 4, PC", Role = "Platform" },
+                }
             };
-     
-            return View();
+
+            return View(game);
         }
     }
 }
